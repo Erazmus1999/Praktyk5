4959696aef361eaf2f997d8969f1ee26300c060d
Johnny <erazmus1999@gmail.com>
2021-08-06 13:21:14 +0200

    Praktyki_2

---
+++ app/src/main/java/pollub/ism/praktyki/DataBase.java
@@ -0,0 +1,36 @@
+package pollub.ism.praktyki;
+
+import android.media.MediaPlayer;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+public class DataBase extends AppCompatActivity
+{
+
+    public void play_Song(View v)
+    {
+    MediaPlayer mediaPlayer = new MediaPlayer();
+
+    try
+    {
+        mediaPlayer.setDataSource(""); //Tu podajemy URL
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+            @Override
+                    public void onPrepared(MediaPlayer mp)
+            {
+                mp.start();
+            }
+        });
+            mediaPlayer.prepare();
+        }
+    catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
